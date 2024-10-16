05209d81ee9e9b99ed4c469406ecf3e1435c9e87
rstam-aloush <167745115+rstam-aloush@users.noreply.github.com>
2024-10-16 09:53:10 +0200

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -18,7 +18,7 @@ function updateLinks(lat, lon) {
     const osmLink = `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lon}#map=18/${lat}/${lon}`;
     const geoUriLink = `geo:${lat},${lon}`;
 
-    const mapBSLink = `https://map.geo.bs.ch/?lang=de&baselayer_ref=Grundkarte%20grau&tree_groups=${tree_groupe}&tree_group_layers_${tree_group_layers_}&map_x=${lat}&map_y=${lon}&map_zoom=12&map_crosshair=true`;
+    const mapBSLink = `https://map.geo.bs.ch/?lang=de&baselayer_ref=Grundkarte%20grau&tree_groups=${tree_groups}&tree_group_layers_${tree_group_layers_}&map_x=${lat}&map_y=${lon}&map_zoom=12&map_crosshair=true`;
 
     document.getElementById('google-maps-link').href = googleMapsLink;
     document.getElementById('apple-maps-link').href = appleMapsLink;
